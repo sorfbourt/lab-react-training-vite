d546d2d31a062c239a4a5b847092e907b9d7ae84
Krystina Witt <krystinawitt@gmail.com>
2023-02-21 18:20:24 +0100

    iteration 8 solution fixed by Daniel

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 function LikeButton() {
-const [likeCount, setLikeCount] = useState(0)
-let colours = ['purple','blue','green','yellow','orange','red']
-const [buttonColor, setbuttonColor] = useState(colours[0])
-const buttonStyle = {backgroundColor: `${buttonColor}`}
+  const [likeCount, setLikeCount] = useState(0);
+  const [colours, setColours] = useState([
+    'purple',
+    'blue',
+    'green',
+    'yellow',
+    'orange',
+    'red',
+  ]);
+  const [buttonColor, setbuttonColor] = useState(colours[0]);
+  const buttonStyle = { backgroundColor: `${buttonColor}` };
 
-const addLike = () =>{
-    setLikeCount(likeCount + 1)
-    
-    const randomColour = Math.floor(Math.random() * (colours.length - 0) + 0)
-    setbuttonColor(colours[randomColour])
-}
+  const addLike = () => {
+    setLikeCount(likeCount + 1);
+    console.log(colours, 'first');
+    colours.push(colours[0]);
+    console.log(colours, 'after push');
+
+    colours.splice(0, 1);
+    console.log(colours, 'after slice');
+
+    setbuttonColor(colours[0]);
+
+    //setbuttonColor(colours[0+likeCount])
+  };
   return (
     <div>
-
-    <button onClick={addLike} style={buttonStyle}>{likeCount} Likes</button>
-
+      <button onClick={addLike} style={buttonStyle}>
+        {likeCount} Likes
+      </button>
     </div>
-  )
+  );
 }
 
-export default LikeButton
+export default LikeButton;
